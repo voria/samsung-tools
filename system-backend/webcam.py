72b5bbd6828873c763808943228471f2b29a9da9
Fortunato Ventre <vorione@gmail.com>
2010-02-25 13:32:32 +0100

    Fix global interface name in system-backend/webcam.py.

--- system-backend/webcam.py
+++ system-backend/webcam.py
@@ -10,7 +10,7 @@ class Webcam(dbus.service.Object):
 	def __init__(self, conn = None, object_path = None, bus_name = None):
 		dbus.service.Object.__init__(self, conn, object_path, bus_name)
 	
-	@dbus.service.method(WEBCAM_INTERFACE_NAME, in_signature = None, out_signature = 'b',
+	@dbus.service.method(SYSTEM_WEBCAM_INTERFACE_NAME, in_signature = None, out_signature = 'b',
 						sender_keyword = 'sender', connection_keyword = 'conn')
 	def IsEnabled(self, sender = None, conn = None):
 		""" Check if webcam is enabled by parsing the output of lsmod. """
