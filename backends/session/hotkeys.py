ff226dc5153023162205a28210c70f17e7932f00
Fortunato Ventre <vorione@gmail.com>
2010-03-01 14:38:36 +0100

    More work on session service's hotkeys module.

--- backends/session/hotkeys.py
+++ backends/session/hotkeys.py
@@ -27,7 +27,6 @@ from backends.globals import *
 from backends.session.util.config import SessionConfig
 
 GCONF_KEY_COMMAND = "/apps/metacity/keybinding_commands/command_"
-GCONF_KEY_COMMAND_SCHEMA = "/schemas/apps/metacity/keybinding_commands/command"
 BACKLIGHT_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "8"
 BLUETOOTH_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "9"
 FAN_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "10"
@@ -35,82 +34,162 @@ WEBCAM_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "11"
 WIRELESS_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "12"
 
 GCONF_KEY_RUN = "/apps/metacity/global_keybindings/run_command_"
-GCONF_KEY_RUN_SCHEMA = "/schemas/apps/metacity/global_keybindings/run_command"
 BACKLIGHT_GCONF_KEY_RUN = GCONF_KEY_RUN + "8"
 BLUETOOTH_GCONF_KEY_RUN = GCONF_KEY_RUN + "9"
 FAN_GCONF_KEY_RUN = GCONF_KEY_RUN + "10"
 WEBCAM_GCONF_KEY_RUN = GCONF_KEY_RUN + "11"
 WIRELESS_GCONF_KEY_RUN = GCONF_KEY_RUN + "12"
 
+BACKLIGHT_COMMAND = "samsung-tools -b toggle --show-notify"
+BLUETOOTH_COMMAND = "samsung-tools -B toggle --show-notify"
+FAN_COMMAND = "samsung-tools -f cycle --show-notify"
+WEBCAM_COMMAND = "samsung-tools -w toggle --show-notify"
+WIRELESS_COMMAND = "samsung-tools -W toggle --show-notify"
+
 class Hotkeys(dbus.service.Object):
 	""" Control hotkeys """
 	def __init__(self, conn = None, object_path = None, bus_name = None):
 		dbus.service.Object.__init__(self, conn, object_path, bus_name)
-		config = SessionConfig(USER_CONFIG_FILE)
-		self.backlight = config.getBacklightHotkey()
-		self.bluetooth = config.getBluetoothHotkey()
-		self.fan = config.getFanHotkey()
-		self.webcam = config.getWebcamHotkey()
-		self.wireless = config.getWirelessHotkey()
+		self.config = SessionConfig(USER_CONFIG_FILE)
+		self.backlight = self.config.getBacklightHotkey()
+		self.bluetooth = self.config.getBluetoothHotkey()
+		self.fan = self.config.getFanHotkey()
+		self.webcam = self.config.getWebcamHotkey()
+		self.wireless = self.config.getWirelessHotkey()
 		## Set hotkeys in gconf
 		gconf_client = gconf.client_get_default()
-		gconf_client.set_string(BACKLIGHT_GCONF_KEY_COMMAND, "samsung-tools -b toggle --show-notify")
-		gconf_client.set_string(BACKLIGHT_GCONF_KEY_RUN, self.backlight)
-		gconf_client.set_string(BLUETOOTH_GCONF_KEY_COMMAND, "samsung-tools -B toggle --show-notify")
-		gconf_client.set_string(BLUETOOTH_GCONF_KEY_RUN, self.bluetooth)
-		gconf_client.set_string(FAN_GCONF_KEY_COMMAND, "samsung-tools -f cycle --show-notify")
-		gconf_client.set_string(FAN_GCONF_KEY_RUN, self.fan)
-		gconf_client.set_string(WEBCAM_GCONF_KEY_COMMAND, "samsung-tools -w toggle --show-notify")
-		gconf_client.set_string(WEBCAM_GCONF_KEY_RUN, self.webcam)
-		gconf_client.set_string(WIRELESS_GCONF_KEY_COMMAND, "samsung-tools -W toggle --show-notify")
-		gconf_client.set_string(WIRELESS_GCONF_KEY_RUN, self.wireless)
+		if self.backlight != None:
+			gconf_client.set_string(BACKLIGHT_GCONF_KEY_COMMAND, BACKLIGHT_COMMAND)
+			gconf_client.set_string(BACKLIGHT_GCONF_KEY_RUN, self.backlight)
+		else:
+			gconf_client.unset(BACKLIGHT_GCONF_KEY_COMMAND)
+			gconf_client.unset(BACKLIGHT_GCONF_KEY_RUN)
+		if self.bluetooth != None:
+			gconf_client.set_string(BLUETOOTH_GCONF_KEY_COMMAND, BLUETOOTH_COMMAND)
+			gconf_client.set_string(BLUETOOTH_GCONF_KEY_RUN, self.bluetooth)
+		else:
+			gconf_client.unset(BLUETOOTH_GCONF_KEY_COMMAND)
+			gconf_client.unset(BLUETOOTH_GCONF_KEY_RUN)
+		if self.fan != None:
+			gconf_client.set_string(FAN_GCONF_KEY_COMMAND, FAN_COMMAND)
+			gconf_client.set_string(FAN_GCONF_KEY_RUN, self.fan)
+		else:
+			gconf_client.unset(FAN_GCONF_KEY_COMMAND)
+			gconf_client.unset(FAN_GCONF_KEY_RUN)
+		if self.webcam != None:
+			gconf_client.set_string(WEBCAM_GCONF_KEY_COMMAND, WEBCAM_COMMAND)
+			gconf_client.set_string(WEBCAM_GCONF_KEY_RUN, self.webcam)
+		else:
+			gconf_client.unset(WEBCAM_GCONF_KEY_COMMAND)
+			gconf_client.unset(WEBCAM_GCONF_KEY_RUN)
+		if self.wireless != None:
+			gconf_client.set_string(WIRELESS_GCONF_KEY_COMMAND, WIRELESS_COMMAND)
+			gconf_client.set_string(WIRELESS_GCONF_KEY_RUN, self.wireless)
+		else:
+			gconf_client.unset(WIRELESS_GCONF_KEY_COMMAND)
+			gconf_client.unset(WIRELESS_GCONF_KEY_RUN)
 	
-	def GetBacklight(self):
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = None, out_signature = 's',
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def GetBacklightHotkey(self, sender = None, conn = None):
+		""" Return the current hotkey for backlight control. """
 		return self.backlight
 	
-	def GetBluetooth(self):
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = None, out_signature = 's',
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def GetBluetoothHotkey(self, sender = None, conn = None):
+		""" Return the current hotkey for bluetooth control. """
 		return self.bluetooth
 	
-	def GetFan(self):
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = None, out_signature = 's',
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def GetFanHotkey(self, sender = None, conn = None):
+		""" Return the current hotkey for fan control. """
 		return self.fan
 	
-	def GetWebcam(self):
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = None, out_signature = 's',
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def GetWebcamHotkey(self, sender = None, conn = None):
+		""" Return the current hotkey for webcam control. """
 		return self.webcam
 	
-	def GetWireless(self):
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = None, out_signature = 's',
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def GetWirelessHotkey(self, sender = None, conn = None):
+		""" Return the current hotkey for wireless control. """
 		return self.wireless 
 	
-	def SetBacklight(self, hotkey):
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = 's', out_signature = None,
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def SetBacklightHotkey(self, hotkey, sender = None, conn = None):
+		""" Set the hotkey for backlight control. """
+		""" If hotkey == None, hotkey will be disabled. """
+		gconf_client = gconf.client_get_default()
+		if hotkey != None:
+			gconf_client.set_string(BACKLIGHT_GCONF_KEY_COMMAND, BACKLIGHT_COMMAND)
+			gconf_client.set_string(BACKLIGHT_GCONF_KEY_RUN, hotkey)
+		else:
+			gconf_client.unset(BACKLIGHT_GCONF_KEY_RUN)
+			gconf_client.unset(BACKLIGHT_GCONF_KEY_COMMAND)
 		self.backlight = hotkey
-		config.setBacklightHotkey(hotkey)
-		# Update hotkey in gconf
-		gconf_client = gconf.client_get_default()
-		gconf_client.set_string(BACKLIGHT_GCONF_KEY_RUN, hotkey)
+		self.config.setBacklightHotkey(hotkey)		
 
-	def SetBluetooth(self, hotkey):
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = 's', out_signature = None,
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def SetBluetoothHotkey(self, hotkey, sender = None, conn = None):
+		""" Set the hotkey for bluetooth control. """
+		""" If hotkey == None, hotkey will be disabled. """
+		gconf_client = gconf.client_get_default()
+		if hotkey != None:
+			gconf_client.set_string(BLUETOOTH_GCONF_KEY_COMMAND, BLUETOOTH_COMMAND)
+			gconf_client.set_string(BLUETOOTH_GCONF_KEY_RUN, hotkey)
+		else:
+			gconf_client.unset(BLUETOOTH_GCONF_KEY_RUN)
+			gconf_client.unset(BLUETOOTH_GCONF_KEY_COMMAND)
 		self.bluetooth = hotkey
-		config.setBluetoothHotkey(hotkey)
-		# Update hotkey in gconf
+		self.config.setBluetoothHotkey(hotkey)
+
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = 's', out_signature = None,
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def SetFanHotkey(self, hotkey, sender = None, conn = None):
+		""" Set the hotkey for CPU fan control. """
+		""" If hotkey == None, hotkey will be disabled. """
 		gconf_client = gconf.client_get_default()
-		gconf_client.set_string(BLUETOOTH_GCONF_KEY_RUN, hotkey)
-	
-	def SetFan(self, hotkey):
+		if hotkey != None:
+			gconf_client.set_string(FAN_GCONF_KEY_COMMAND, FAN_COMMAND)
+			gconf_client.set_string(FAN_GCONF_KEY_RUN, hotkey)
+		else:
+			gconf_client.unset(FAN_GCONF_KEY_RUN)
+			gconf_client.unset(FAN_GCONF_KEY_COMMAND)
 		self.fan = hotkey
-		config.setFanHotkey(hotkey)
-		# Update hotkey in gconf
+		self.config.setFanHotkey(hotkey)
+
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = 's', out_signature = None,
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def SetWebcamHotkey(self, hotkey, sender = None, conn = None):
+		""" Set the hotkey for webcam control. """
+		""" If hotkey == None, hotkey will be disabled. """
 		gconf_client = gconf.client_get_default()
-		gconf_client.set_string(FAN_GCONF_KEY_RUN, hotkey)
-	
-	def SetWebcam(self, hotkey):
+		if hotkey != None:
+			gconf_client.set_string(WEBCAM_GCONF_KEY_COMMAND, WEBCAM_COMMAND)
+			gconf_client.set_string(WEBCAM_GCONF_KEY_RUN, hotkey)
+		else:
+			gconf_client.unset(WEBCAM_GCONF_KEY_RUN)
+			gconf_client.unset(WEBCAM_GCONF_KEY_COMMAND)
 		self.webcam = hotkey
-		config.setWebcamHotkey(hotkey)
-		# Update hotkey in gconf
-		gconf_client = gconf.client_get_default()
-		gconf_client.set_string(WEBCAM_GCONF_KEY_RUN, hotkey)
+		self.config.setWebcamHotkey(hotkey)
 	
-	def SetWireless(self, hotkey):
-		self.wireless = hotkey
-		config.setWirelessHotkey(hotkey)
-		# Update hotkey in gconf
+	@dbus.service.method(SESSION_INTERFACE_NAME, in_signature = 's', out_signature = None,
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def SetWirelessHotkey(self, hotkey, sender = None, conn = None):
+		""" Set the hotkey for wireless control. """
+		""" If hotkey == None, hotkey will be disabled. """
 		gconf_client = gconf.client_get_default()
-		gconf_client.set_string(WIRELESS_GCONF_KEY_RUN, hotkey)
+		if hotkey != None:
+			gconf_client.set_string(WIRELESS_GCONF_KEY_COMMAND, WIRELESS_COMMAND)
+			gconf_client.set_string(WIRELESS_GCONF_KEY_RUN, hotkey)
+		else:
+			gconf_client.unset(WIRELESS_GCONF_KEY_RUN)
+			gconf_client.unset(WIRELESS_GCONF_KEY_COMMAND)
+		self.wireless = hotkey
+		self.config.setWirelessHotkey(hotkey)
