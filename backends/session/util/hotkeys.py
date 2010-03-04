0357a17d81bc9e433b24393990ff20fb068a91b5
Fortunato Ventre <vorione@gmail.com>
2010-03-04 18:29:47 +0100

    Initial work on hotkeys support.

--- backends/session/util/hotkeys.py
+++ backends/session/util/hotkeys.py
@@ -19,29 +19,11 @@
 # See the GNU General Public License for more details.
 # <http://www.gnu.org/licenses/gpl.txt>
 
+import os
+import shutil
+import subprocess
+
 from backends.globals import *
-
-try:
-	import gconf
-except:
-	log_session.write("ERROR: 'hotkeys'  - cannot import gconf.")
-	pass
-
-import dbus.service
-
-GCONF_KEY_COMMAND = "/apps/metacity/keybinding_commands/command_"
-BACKLIGHT_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "8"
-BLUETOOTH_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "9"
-FAN_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "10"
-WEBCAM_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "11"
-WIRELESS_GCONF_KEY_COMMAND = GCONF_KEY_COMMAND + "12"
-
-GCONF_KEY_RUN = "/apps/metacity/global_keybindings/run_command_"
-BACKLIGHT_GCONF_KEY_RUN = GCONF_KEY_RUN + "8"
-BLUETOOTH_GCONF_KEY_RUN = GCONF_KEY_RUN + "9"
-FAN_GCONF_KEY_RUN = GCONF_KEY_RUN + "10"
-WEBCAM_GCONF_KEY_RUN = GCONF_KEY_RUN + "11"
-WIRELESS_GCONF_KEY_RUN = GCONF_KEY_RUN + "12"
 
 BACKLIGHT_COMMAND = "samsung-tools -b toggle --show-notify"
 BLUETOOTH_COMMAND = "samsung-tools -B toggle --show-notify"
@@ -49,56 +31,122 @@ FAN_COMMAND = "samsung-tools -f hotkey --show-notify"
 WEBCAM_COMMAND = "samsung-tools -w toggle --show-notify"
 WIRELESS_COMMAND = "samsung-tools -W toggle --show-notify"
 
-class Hotkeys(dbus.service.Object):
-	""" Control hotkeys """
-	def __init__(self, conn = None, object_path = None, bus_name = None):
-		dbus.service.Object.__init__(self, conn, object_path, bus_name)
-		self.config = SessionConfig(USER_CONFIG_FILE)
-		self.backlight = self.config.getBacklightHotkey()
-		self.bluetooth = self.config.getBluetoothHotkey()
-		self.fan = self.config.getFanHotkey()
-		self.webcam = self.config.getWebcamHotkey()
-		self.wireless = self.config.getWirelessHotkey()
-		## Set hotkeys in gconf
-		try:
-			gconf_client = gconf.client_get_default()
-			if self.backlight != "none":
-				gconf_client.set_string(BACKLIGHT_GCONF_KEY_COMMAND, BACKLIGHT_COMMAND)
-				gconf_client.set_string(BACKLIGHT_GCONF_KEY_RUN, self.backlight)
-			else:
-				if gconf_client.get_string(BACKLIGHT_GCONF_KEY_COMMAND) == BACKLIGHT_COMMAND:
-					gconf_client.unset(BACKLIGHT_GCONF_KEY_COMMAND)
-					gconf_client.unset(BACKLIGHT_GCONF_KEY_RUN)
-			if self.bluetooth != "none":
-				gconf_client.set_string(BLUETOOTH_GCONF_KEY_COMMAND, BLUETOOTH_COMMAND)
-				gconf_client.set_string(BLUETOOTH_GCONF_KEY_RUN, self.bluetooth)
-			else:
-				if gconf_client.get_string(BLUETOOTH_GCONF_KEY_COMMAND) == BLUETOOTH_COMMAND:
-					gconf_client.unset(BLUETOOTH_GCONF_KEY_COMMAND)
-					gconf_client.unset(BLUETOOTH_GCONF_KEY_RUN)
-			if self.fan != "none":
-				gconf_client.set_string(FAN_GCONF_KEY_COMMAND, FAN_COMMAND)
-				gconf_client.set_string(FAN_GCONF_KEY_RUN, self.fan)
-			else:
-				if gconf_client.get_string(FAN_GCONF_KEY_COMMAND) == FAN_COMMAND:
-					gconf_client.unset(FAN_GCONF_KEY_COMMAND)
-					gconf_client.unset(FAN_GCONF_KEY_RUN)
-			if self.webcam != "none":
-				gconf_client.set_string(WEBCAM_GCONF_KEY_COMMAND, WEBCAM_COMMAND)
-				gconf_client.set_string(WEBCAM_GCONF_KEY_RUN, self.webcam)
-			else:
-				if gconf_client.get_string(WEBCAM_GCONF_KEY_COMMAND) == WEBCAM_COMMAND:
-					gconf_client.unset(WEBCAM_GCONF_KEY_COMMAND)
-					gconf_client.unset(WEBCAM_GCONF_KEY_RUN)
-			if self.wireless != "none":
-				gconf_client.set_string(WIRELESS_GCONF_KEY_COMMAND, WIRELESS_COMMAND)
-				gconf_client.set_string(WIRELESS_GCONF_KEY_RUN, self.wireless)
-			else:
-				if gconf_client.get_string(WIRELESS_GCONF_KEY_COMMAND) == WIRELESS_COMMAND:
-					gconf_client.unset(WIRELESS_GCONF_KEY_COMMAND)
-					gconf_client.unset(WIRELESS_GCONF_KEY_RUN)
-		except:
-			log_session.write("ERROR: 'Hotkeys()'  - cannot set hotkeys in gconf.")
-			pass
-		
+BACKLIGHT_HOTKEY_DEFAULT = "XF86Launch1"
+BLUETOOTH_HOTKEY_DEFAULT = "XF86Launch2"
+FAN_HOTKEY_DEFAULT = "XF86Launch3"
+WEBCAM_HOTKEY_DEFAULT = "Alt + KP_Insert"
+WIRELESS_HOTKEY_DEFAULT = "XF86WLAN"
+
+XBINDKEYS_CONFIG_FILE = os.path.join(os.getenv('HOME'), ".xbindkeysrc")
+
+class Hotkeys():
+	def __init__(self):
+		if not os.path.exists(XBINDKEYS_CONFIG_FILE):
+			# Create the configuration file
+			file = open(XBINDKEYS_CONFIG_FILE, 'w')
+			file.close()
+			# Set default hotkeys
+			self.setBacklightHotkey("default")
+			self.setBluetoothHotkey("default")
+			self.setFanHotkey("default")
+			self.setWebcamHotkey("default")
+			self.setWirelessHotkey("default")
 	
+	def __get_hotkey(self, command):
+		""" Return the hotkey for 'command', 'None' if not found. """
+		hotkey = None
+		found = False
+		file = open(XBINDKEYS_CONFIG_FILE, "r")
+		for line in file:
+			if found == True:
+				hotkey = line.strip()
+				break
+			if line == '"' + command + '"\n':
+				found = True
+		file.close()
+		return hotkey
+	
+	def __update_hotkey(self, command, hotkey):
+		""" Update the hotkey for 'command' to 'hotkey'. """
+		""" If 'command' is not found, add it with the new 'hotkey'. """
+		oldfile = open(XBINDKEYS_CONFIG_FILE, "r")
+		newfile = open(XBINDKEYS_CONFIG_FILE + ".new", "w")
+		# Search for command
+		commandfound = False
+		skipnextline = False
+		for line in oldfile:
+			if skipnextline == False:
+				newfile.write(line)
+			else:
+				skipnextline = False
+			if line == '"' + command + '"\n':
+				newfile.write("  " + hotkey + "\n") # update hotkey
+				commandfound = True
+				skipnextline = True
+		if commandfound == False:
+			# command not found, add it
+			newfile.write('"' + command + '"\n')
+			newfile.write("  " + hotkey + "\n")
+		oldfile.close()
+		newfile.close()
+		os.remove(XBINDKEYS_CONFIG_FILE)
+		shutil.move(XBINDKEYS_CONFIG_FILE + ".new", XBINDKEYS_CONFIG_FILE)
+	
+	def startHotkeys(self):
+		# Check if xbindkeys is already started
+		process = subprocess.Popen(['ps', 'x'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+		output = process.communicate()[0].split()
+		if "xbindkeys" in output:
+			# already enabled
+			return
+		process = subprocess.Popen(['xbindkeys'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+		process.communicate()
+	
+	def stopHotkeys(self):
+		process = subprocess.Popen(['killall', 'xbindkeys'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+		process.communicate()
+	
+	def restartHotkeys(self):
+		self.disableHotkeys()
+		self.enableHotkeys()
+	
+	def getBacklightHotkey(self):
+		return self.__get_hotkey(BACKLIGHT_COMMAND)
+	
+	def getBluetoothHotkey(self):
+		return self.__get_hotkey(BLUETOOTH_COMMAND)
+	
+	def getFanHotkey(self):
+		return self.__get_hotkey(FAN_COMMAND)
+	
+	def getWebcamHotkey(self):
+		return self.__get_hotkey(WEBCAM_COMMAND)
+	
+	def getWirelessHotkey(self):
+		return self.__get_hotkey(WIRELESS_COMMAND)
+	
+	def setBacklightHotkey(self, hotkey):
+		if hotkey == "default":
+			hotkey = BACKLIGHT_HOTKEY_DEFAULT
+		self.__update_hotkey(BACKLIGHT_COMMAND, hotkey)
+	
+	def setBluetoothHotkey(self, hotkey):
+		if hotkey == "default":
+			hotkey = BLUETOOTH_HOTKEY_DEFAULT
+		self.__update_hotkey(BLUETOOTH_COMMAND, hotkey)
+	
+	def setFanHotkey(self, hotkey):
+		if hotkey == "default":
+			hotkey = FAN_HOTKEY_DEFAULT
+		self.__update_hotkey(FAN_COMMAND, hotkey)
+		
+	def setWebcamHotkey(self, hotkey):
+		if hotkey == "default":
+			hotkey = WEBCAM_HOTKEY_DEFAULT
+		self.__update_hotkey(WEBCAM_COMMAND, hotkey)
+		
+	def setWirelessHotkey(self, hotkey):
+		if hotkey == "default":
+			hotkey = WIRELESS_HOTKEY_DEFAULT
+		self.__update_hotkey(WIRELESS_COMMAND, hotkey)
+
