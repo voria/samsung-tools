4bace80c45d6194b0811bf8539b448a8ef5b7369
Fortunato Ventre <vorione@gmail.com>
2010-03-07 19:45:24 +0100

    Attempt to translate .desktop resources too.

--- po/update.sh
+++ po/update.sh
@@ -2,17 +2,26 @@
 # This script creates a new template 'samsung-tools.pot', and updates all translations against it
 
 echo "*** Extracting strings from *.glade files..."
-intltool-extract --type=gettext/glade gui/glade/*.glade
+for file in gui/glade/*.glade; do
+	intltool-extract --type=gettext/glade "$file"
+done
 mv gui/glade/*.glade.h po/
+
+echo "*** Extracting strings from *.desktop files..." # A bit hacking, but it works
+for file in desktop/*.desktop; do
+	intltool-extract --type=gettext/ini "$file"
+done
+mv desktop/*.desktop.h po/
 
 echo
 echo "*** Creating samsung-tools.pot..."
-xgettext -k_ -kN_ -o po/messages.pot `cat po/FILES` po/*.glade.h
+xgettext -k_ -kN_ -o po/messages.pot `cat po/FILES` po/*.glade.h po/*.desktop.h
 cat po/messages.pot | sed s:charset=CHARSET:charset=UTF-8: > po/samsung-tools.pot
 
 echo
 echo "*** Removing unneeded files..."
 rm po/*.glade.h
+rm po/*.desktop.h
 rm po/messages.pot
 
 for locale in `cat po/LINGUAS`; do
