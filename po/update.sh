c84a61882066601f21d46fe4e1d75c93b53be680
Fortunato Ventre <vorione@gmail.com>
2010-03-07 13:22:29 +0100

    Rename 'messages.pot' as 'samsung-tools.pot'.

--- po/update.sh
+++ po/update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# This script creates a new template 'samsung-tools.pot', and updates all translations against it
 
 echo "*** Extracting strings from *.glade files..."
 intltool-extract --type=gettext/glade gui/glade/*.glade
 mv gui/glade/*.glade.h .
 
 echo
-echo "*** Creating messages.pot..."
-xgettext -k_ -kN_ -o po/messages.pot `cat po/FILES` *.glade.h
+echo "*** Creating samsung-tools.pot..."
+xgettext -k_ -kN_ -o po/samsung-tools.pot `cat po/FILES` *.glade.h
 
 echo
 echo "*** Removing unneeded files..."
@@ -15,12 +16,9 @@ rm *.glade.h
 for locale in `cat po/LINGUAS`; do
 	if [ -f po/$locale.po ]; then
 		echo "*** Updating '`echo $locale.po | cut -d/ -f2`'..."
-		msgmerge -U po/$locale.po po/messages.pot
+		msgmerge -U po/$locale.po po/samsung-tools.pot
 	else
 		echo "*** Creating new '`echo $locale.po | cut -d/ -f2`'..."
-		cp po/messages.pot po/$locale.po
+		cp po/samsung-tools.pot po/$locale.po
 	fi
 done
-
-echo
-echo "Done."
