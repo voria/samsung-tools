ab8555f07bf47f530aa9c315ff66590fc457d0f0
Fortunato Ventre (voRia) <vorione@gmail.com>
2010-05-20 20:23:04 +0200

    Rewrite SysCtl() class in order to use '/etc/sysctl.conf' configuration file only.

--- backends/system/powermanagement.py
+++ backends/system/powermanagement.py
@@ -36,7 +36,8 @@ LM_USBAUTOSUSPEND_CONFIG_FILE = "/etc/laptop-mode/conf.d/usb-autosuspend.conf"
 LM_VIDEOOUT_CONFIG_FILE = "/etc/laptop-mode/conf.d/video-out.conf"
 
 # sysctl configuration file
-SYSCTL_CONFIG_FILE = "/etc/sysctl.d/30-samsung-tools.conf"
+OLD_SYSCTL_CONFIG_FILE = "/etc/sysctl.d/30-samsung-tools.conf"
+SYSCTL_CONFIG_FILE = "/etc/sysctl.conf"
 
 class LaptopMode(dbus.service.Object):
 	""" Manage laptop-mode """
@@ -321,6 +322,84 @@ class SysCtl(dbus.service.Object):
 	""" Manage sysctl options """
 	def __init__(self, conn = None, object_path = None, bus_name = None):
 		dbus.service.Object.__init__(self, conn, object_path, bus_name)
+		self.available = self.IsAvailable()
+	
+	def __write(self, file, option, value):
+		""" Write the 'option = value' in the 'file' configfile. """
+		""" If 'option' is not found in the config file, add it. """
+		""" Return "True" on success, "False" otherwise. """
+		if not self.available:
+			return False
+		
+		optionfound = False
+		try:
+			oldfile = open(file, "r")
+		except:
+			systemlog.write("ERROR: 'SysCtl.__write()' - '" + file + "' not found.")
+			return False
+		try:
+			newfile = open(file + ".new", "w")
+		except:
+			systemlog.write("ERROR: 'SysCtl.__write()' - cannot write the new config file.")
+			oldfile.close()
+			return False
+		for line in oldfile:
+			if line[0:1] == "#" or line == "\n":
+				newfile.write(line)
+			else:
+				currentoption = line.split('=')[0].strip()
+				if currentoption != option: # not the option we are searching for
+					newfile.write(line)
+				else:
+					optionfound = True
+					try:					
+						newfile.write(option + " = " + value + "\n")
+					except:
+						systemlog.write("ERROR: 'SysCtl.__write()' - cannot write the new value for '" + option + "' in the new config file.")
+						oldfile.close()
+						newfile.close()
+						os.remove(file + ".new")
+						return False
+		oldfile.close()
+		if optionfound == False: # option not found in current config file, add it
+			newfile.write(option + " = " + value + "\n")
+		newfile.close()
+		try:
+			os.remove(file)
+		except:
+			systemlog.write("ERROR: 'SysCtl.__write()' - cannot replace old '" + file + "' with the new version.")
+			os.remove(file + ".new")
+			return False
+		shutil.move(file + ".new", file)
+		return True
+	
+	def __read(self, file, option):
+		""" Read the 'option' value in the 'file' configfile. """
+		""" Return the read value, or 'None' if any error. """
+		if not self.available:
+			return None
+		try:
+			f = open(file, "r")
+		except:
+			systemlog.write("ERROR: 'SysCtl.__read()' - '" + file + "' not found.")
+			return None
+		for line in f:
+			if line[0:1] != "#" and line != "\n":
+				currentoption = line.split('=')[0].strip()
+				if currentoption == option:
+					f.close()
+					return line.split('=')[1].strip()
+		# Option not found
+		f.close()
+		return None
+	
+	@dbus.service.method(SYSTEM_INTERFACE_NAME, in_signature = None, out_signature = 'b',
+						sender_keyword = 'sender', connection_keyword = 'conn')
+	def IsAvailable(self, sender = None, conn = None):
+		if os.path.exists(SYSCTL_CONFIG_FILE):
+			return True
+		else:
+			return False
 	
 	@dbus.service.method(SYSTEM_INTERFACE_NAME, in_signature = None, out_signature = 'i',
 						sender_keyword = 'sender', connection_keyword = 'conn')
@@ -333,23 +412,21 @@ class SysCtl(dbus.service.Object):
 	@dbus.service.method(SYSTEM_INTERFACE_NAME, in_signature = 'i', out_signature = 'b',
 						sender_keyword = 'sender', connection_keyword = 'conn')
 	def SetSwappiness(self, value, sender = None, conn = None):
+		# Make sure old configuration file is removed
+		if os.path.exists(OLD_SYSCTL_CONFIG_FILE):
+			os.remove(OLD_SYSCTL_CONFIG_FILE)
 		if value < 0 or value > 100:
 			return False
-		try:
-			f = open(SYSCTL_CONFIG_FILE, "w")
-			f.write("vm.swappiness = " + str(value) + "\n")
-			f.close()
-			return True
-		except:
-			systemlog.write("ERROR: 'SysCtl.SetSwappiness()' - cannot write the new config file.")
-			return False
+		return self.__write(SYSCTL_CONFIG_FILE, "vm.swappiness", str(value))
 	
 	@dbus.service.method(SYSTEM_INTERFACE_NAME, in_signature = None, out_signature = 'b',
 						sender_keyword = 'sender', connection_keyword = 'conn')
 	def ApplySettings(self, sender = None, conn = None):
 		from subprocess import Popen, PIPE
 		try:
-			command = "/etc/init.d/procps start"
+			value = self.__read(SYSCTL_CONFIG_FILE, "vm.swappiness")
+			if value != None:
+				command = COMMAND_SYSCTL + " vm.swappiness=" + value
 			process = Popen(command.split(), stdout = PIPE, stderr = PIPE)
 			process.communicate()
 			if process.returncode != 0:
