7e6c31dda49dcba421892dc036497e4109c432c2
Fortunato Ventre (voRia) <vorione@gmail.com>
2010-05-20 22:33:09 +0200

    Remove "ubuntu-xgettext" extension support from .desktop and .server files because it's not available in other distros. Modify prepare.sh scripts in order to create old-style files.

--- bonobo/prepare.sh
+++ bonobo/prepare.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 # This script is intended to be called from Makefile, it prepares .server files
+# It's a bit hackish but it works
 
 FILE="$1"
 NEWFILE=`echo $FILE | sed 's:.in::'`
 
-cat $FILE | sed 's:_value:value:' > $NEWFILE
+# Get the translatable string
+VALUE=`grep _value "$FILE" | sed 's:.*_value=::' | cut -d\" -f2`
+
+# The line where the translatable string has been found
+VALUELINE=`grep _value "$FILE"`
+
+# Remove the underscore and create the first part of the new .server file
+sed -n '1,/.*_value/p' "$FILE" | sed 's/_value/value/' > $NEWFILE
+
+# Add translations
+if [ -n "$VALUE" ]; then
+	# For each language specified in po/LINGUAS, take the translation from the .po file and
+	# add it to the new .desktop file as Name[language]
+	for lang in `cat po/LINGUAS`; do
+		TRANSLATION=`msggrep --msgid -F -e "$VALUE" po/$lang.po | tac | sed -n '1p' | cut -d\" -f 2`
+		if [ -n "$TRANSLATION" ]; then
+			echo "$VALUELINE" | sed "s/description/description-$lang/" | sed "s/_value/value/" | sed "s/$VALUE/$TRANSLATION/" >> $NEWFILE
+		fi
+	done
+fi
+
+# Write the remaining part of the .server file
+sed -n '/.*_value/,/END/p' "$FILE" | sed -n '2,/END/p' >> $NEWFILE
--- desktop/prepare.sh
+++ desktop/prepare.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 # This script is intended to be called from Makefile, it prepares .desktop files
+# It's a bit hackish but it works
 
 FILE="$1"
 NEWFILE=`echo $FILE | sed 's:.in::'`
 
+# Get the translatable strings
+NAME=`grep ^_Name "$FILE" | cut -d= -f2`
+COMMENT=`grep ^_Comment "$FILE" | cut -d= -f2`
+
+# Remove the underscores and create the new .desktop file
 cat $FILE | sed 's:_Name:Name:' | sed 's:_Comment:Comment:' > $NEWFILE
+
+# Add translations for "Name="
+if [ -n "$NAME" ]; then
+	# For each language specified in po/LINGUAS, take the translation from the .po file and
+	# add it to the new .desktop file as Name[language]
+	for lang in `cat po/LINGUAS`; do
+		TRANSLATION=`msggrep --msgid -F -e "$NAME" po/$lang.po | tac | sed -n '1p' | cut -d\" -f 2`
+		if [ -n "$TRANSLATION" ]; then
+			echo -n "Name[$lang]=" >> $NEWFILE
+			echo "$TRANSLATION" >> $NEWFILE
+		fi
+	done
+fi
+
+# Add translations for "Comment="
+if [ -n "$COMMENT" ]; then
+	# For each language specified in po/LINGUAS, take the translation from the .po file and
+	# add it to the new .desktop file as Comment[language]
+	for lang in `cat po/LINGUAS`; do
+		TRANSLATION=`msggrep --msgid -F -e "$COMMENT" po/$lang.po | tac | sed -n '1p' | cut -d\" -f 2`
+		if [ -n "$TRANSLATION" ]; then
+			echo -n "Comment[$lang]=" >> $NEWFILE
+			echo "$TRANSLATION" >> $NEWFILE
+		fi
+	done
+fi
