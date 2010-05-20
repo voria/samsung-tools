80ef2db44ffbd04013dce25b305f69dbd37791e9
Fortunato Ventre (voRia) <vorione@gmail.com>
2010-05-20 18:38:24 +0200

    Reorganize globals.py content.

--- backends/globals.py
+++ backends/globals.py
@@ -25,10 +25,7 @@ APP_NAME = "Samsung Tools"
 APP_VERSION = "1.0.3"
 WORK_DIRECTORY = "/usr/lib/samsung-tools/"
 
-###
-### Session service
-###
-# Interface/Objects
+# Interface/Objects for session service
 SESSION_INTERFACE_NAME = "org.voria.SamsungTools.Session"
 SESSION_OBJECT_PATH_GENERAL = "/"
 SESSION_OBJECT_PATH_OPTIONS = "/Options"
@@ -37,20 +34,7 @@ SESSION_OBJECT_PATH_BLUETOOTH = "/Device/Bluetooth"
 SESSION_OBJECT_PATH_CPU = "/Device/Cpu"
 SESSION_OBJECT_PATH_WEBCAM = "/Device/Webcam"
 SESSION_OBJECT_PATH_WIRELESS = "/Device/Wireless"
-# Config/Log
-SESSION_CONFIG_FILE = "/etc/samsung-tools/session.conf"
-import os
-USER_DIRECTORY = os.path.join(os.getenv('HOME', '/root'), ".samsung-tools")
-USER_CONFIG_FILE = os.path.join(USER_DIRECTORY, os.path.basename(SESSION_CONFIG_FILE))
-SESSION_LOG_FILE = os.path.join(USER_DIRECTORY, "log")
-sessionlog = Log(SESSION_LOG_FILE)
-from backends.session.util.config import SessionConfig
-sessionconfig = SessionConfig(USER_CONFIG_FILE)
-
-###
-### System service
-###
-# Interface/Objects
+# Interface/Objects for system service
 SYSTEM_INTERFACE_NAME = "org.voria.SamsungTools.System"
 SYSTEM_OBJECT_PATH_GENERAL = "/"
 SYSTEM_OBJECT_PATH_OPTIONS = "/Options"
@@ -62,32 +46,51 @@ SYSTEM_OBJECT_PATH_CPU = "/Device/Cpu"
 SYSTEM_OBJECT_PATH_FAN = "/Device/Fan"
 SYSTEM_OBJECT_PATH_WEBCAM = "/Device/Webcam"
 SYSTEM_OBJECT_PATH_WIRELESS = "/Device/Wireless"
-# Config/Log
-SYSTEM_CONFIG_FILE = "/etc/samsung-tools/system.conf"
-SYSTEM_LOG_FILE = "/var/log/samsung-tools.log"
-systemlog = Log(SYSTEM_LOG_FILE)
-from backends.system.util.config import SystemConfig
-systemconfig = SystemConfig(SYSTEM_CONFIG_FILE)
-# Last devices' status files
-LAST_DEVICES_STATUS_DIRECTORY = os.path.join(WORK_DIRECTORY, "devices-status")
-LAST_DEVICE_STATUS_BACKLIGHT = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "backlight")
-LAST_DEVICE_STATUS_BLUETOOTH = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "bluetooth") 
-LAST_DEVICE_STATUS_WEBCAM = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "webcam")
-LAST_DEVICE_STATUS_WIRELESS = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "wireless")
-LAST_DEVICE_STATUS_CPUFAN = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "cpufan")
-# Commands:
-# system service needs commands to be specified with absolute paths,
-# or else it will fail to find them.
-COMMAND_MODPROBE = "/sbin/modprobe"
-COMMAND_VBETOOL = "/usr/sbin/vbetool"
-COMMAND_LSMOD = "/sbin/lsmod"
-COMMAND_DMESG = "/bin/dmesg"
-COMMAND_RFKILL = "/sbin/rfkill"
-COMMAND_SYSCTL = "/sbin/sysctl"
-# Easy slow down manager interface
-ESDM_MODULE = "easy_slow_down_manager"
-ESDM_PATH_BACKLIGHT = "/proc/easy_backlight"
-ESDM_PATH_FAN = "/proc/easy_slow_down_manager"
-ESDM_PATH_WIRELESS = "/proc/easy_wifi_kill"
-# Temperature
-CPU_TEMPERATURE_PATH = "/proc/acpi/thermal_zone/TZ00/temperature"
+
+import sys, os.path
+###
+### Stuff for session service only
+###
+if os.path.basename(sys.argv[0]) == "session-service.py":
+	# Config/Log
+	SESSION_CONFIG_FILE = "/etc/samsung-tools/session.conf"
+	USER_DIRECTORY = os.path.join(os.getenv('HOME'), ".samsung-tools")
+	USER_CONFIG_FILE = os.path.join(USER_DIRECTORY, os.path.basename(SESSION_CONFIG_FILE))
+	SESSION_LOG_FILE = os.path.join(USER_DIRECTORY, "log")
+	sessionlog = Log(SESSION_LOG_FILE)
+	from backends.session.util.config import SessionConfig
+	sessionconfig = SessionConfig(USER_CONFIG_FILE)
+
+###
+### Stuff for system service only
+###
+if os.path.basename(sys.argv[0]) == "system-service.py":
+	# Config/Log
+	SYSTEM_CONFIG_FILE = "/etc/samsung-tools/system.conf"
+	SYSTEM_LOG_FILE = "/var/log/samsung-tools.log"
+	systemlog = Log(SYSTEM_LOG_FILE)
+	from backends.system.util.config import SystemConfig
+	systemconfig = SystemConfig(SYSTEM_CONFIG_FILE)
+	# Last devices' status files
+	LAST_DEVICES_STATUS_DIRECTORY = os.path.join(WORK_DIRECTORY, "devices-status")
+	LAST_DEVICE_STATUS_BACKLIGHT = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "backlight")
+	LAST_DEVICE_STATUS_BLUETOOTH = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "bluetooth") 
+	LAST_DEVICE_STATUS_WEBCAM = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "webcam")
+	LAST_DEVICE_STATUS_WIRELESS = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "wireless")
+	LAST_DEVICE_STATUS_CPUFAN = os.path.join(LAST_DEVICES_STATUS_DIRECTORY, "cpufan")
+	# Commands:
+	# system service needs commands to be specified with absolute paths,
+	# or else it will fail to find them.
+	COMMAND_MODPROBE = "/sbin/modprobe"
+	COMMAND_VBETOOL = "/usr/sbin/vbetool"
+	COMMAND_LSMOD = "/sbin/lsmod"
+	COMMAND_DMESG = "/bin/dmesg"
+	COMMAND_RFKILL = "/sbin/rfkill"
+	COMMAND_SYSCTL = "/sbin/sysctl"
+	# Easy slow down manager interface
+	ESDM_MODULE = "easy_slow_down_manager"
+	ESDM_PATH_BACKLIGHT = "/proc/easy_backlight"
+	ESDM_PATH_FAN = "/proc/easy_slow_down_manager"
+	ESDM_PATH_WIRELESS = "/proc/easy_wifi_kill"
+	# Temperature
+	CPU_TEMPERATURE_PATH = "/proc/acpi/thermal_zone/TZ00/temperature"
