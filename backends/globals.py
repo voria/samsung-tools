206ef43553ec30093cb84e163c4b83b7474edfe1
Fortunato Ventre (voRia) <vorione@gmail.com>
2011-05-04 20:26:49 +0200

    Update version

--- backends/globals.py
+++ backends/globals.py
@@ -22,7 +22,7 @@
 from backends.log import Log
 
 APP_NAME = "Samsung Tools"
-APP_VERSION = "1.3.2"
+APP_VERSION = "1.3.3-dev"
 WORK_DIRECTORY = "/usr/lib/samsung-tools/"
 
 # Interface/Objects for session service
