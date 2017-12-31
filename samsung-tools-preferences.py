voria/samsung-tools

commit a0a7b326ec1ba72f379ab730f01d697ec86a2592

Various PEP8 fixes

samsung-tools-preferences.py
@@ -24,19 +24,18 @@ import time
 import gtk
 import gobject
 import dbus
-
-WORK_DIRECTORY = "/usr/share/samsung-tools"
-
-import sys
-sys.path.append(WORK_DIRECTORY)
-
 import gettext
-_ = gettext.gettext
-gettext.bindtextdomain("samsung-tools")
-gettext.textdomain("samsung-tools")
+import sys
 
 from backends.globals import *
 from backends.session.util.icons import *
+
+WORK_DIRECTORY = "/usr/share/samsung-tools"
+sys.path.append(WORK_DIRECTORY)
+
+_ = gettext.gettext
+gettext.bindtextdomain("samsung-tools")
+gettext.textdomain("samsung-tools")
 
 
 class Popup (gtk.Window):
@@ -399,7 +398,7 @@ class PhcDialog():
             if response == gtk.RESPONSE_YES:
                 conn.SetCurrentVids(newvids)
         conn = self.__connect_options()
-        if self.applyAtBootCheckbutton.get_active() == True:
+        if self.applyAtBootCheckbutton.get_active():
             conn.SetPHCVids(newvids)
         else:
             conn.SetPHCVids("default")
@@ -963,9 +962,9 @@ class Main():
         self.__set_webcam_hotkey_sensitiveness(checkbutton.get_active())
         self.__set_wireless_hotkey_sensitiveness(checkbutton.get_active())
 
-        if toggle_widgets_only == False:
+        if not toggle_widgets_only:
             session = self.__connect_session_options()
-            if checkbutton.get_active() == True:
+            if checkbutton.get_active():
                 session.SetUseHotkeys("true")
             else:
                 session.SetUseHotkeys("false")

samsung-tools.py
@@ -21,24 +21,26 @@
 
 import os
 import sys
-WORK_DIRECTORY = "/usr/share/samsung-tools"
-sys.path.append(WORK_DIRECTORY)
 
 from optparse import OptionParser
 import dbus
+import codecs
 
 import gettext
-_ = gettext.gettext
-gettext.bindtextdomain("samsung-tools")
-gettext.textdomain("samsung-tools")
 
 from backends.globals import *
 from backends.session.util.locales import *
 
+WORK_DIRECTORY = "/usr/share/samsung-tools"
+sys.path.append(WORK_DIRECTORY)
+
+_ = gettext.gettext
+gettext.bindtextdomain("samsung-tools")
+gettext.textdomain("samsung-tools")
+
 quiet = False
 
 # Fix encoding for piping
-import codecs
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 
@@ -48,7 +50,7 @@ class Backlight():
         self.option = option
         success = False
         retry = 3
-        while retry > 0 and success == False:
+        while retry > 0 and not success:
             try:
                 bus = dbus.SessionBus()
                 proxy = bus.get_object(
@@ -80,23 +82,23 @@ class Backlight():
         if self.option == "on":
             result = self.__on()
             if not quiet:
-                if result == True:
+                if result:
                     print BACKLIGHT_ENABLED
                 else:
                     print BACKLIGHT_ENABLING_ERROR
         if self.option == "off":
             result = self.__off()
             if not quiet:
-                if result == True:
+                if result:
                     print BACKLIGHT_DISABLED
                 else:
                     print BACKLIGHT_DISABLING_ERROR
         if self.option == "toggle":
             result = self.__toggle()
             if not quiet:
-                if result == True:
+                if result:
                     status = self.__status()
-                    if status == True:
+                    if status:
                         print BACKLIGHT_ENABLED
                     else:
                         print BACKLIGHT_DISABLED
@@ -115,7 +117,7 @@ class Backlight():
                     toggle = False
             except:
                 pass
-            if toggle == True:
+            if toggle:
                 Backlight("toggle").apply()
                 try:
                     file = open(tempfile, "w").close()  # create temp file
@@ -129,7 +131,7 @@ class Backlight():
         if self.option == "status":
             result = self.__status()
             if not quiet:
-                if result == True:
+                if result:
                     print BACKLIGHT_STATUS_ENABLED
                 else:
                     print BACKLIGHT_STATUS_DISABLED
@@ -142,7 +144,7 @@ class Bluetooth():
         self.use_notify = use_notify
         success = False
         retry = 3
-        while retry > 0 and success == False:
+        while retry > 0 and not success:
             try:
                 bus = dbus.SessionBus()
                 proxy = bus.get_object(
@@ -182,28 +184,28 @@ class Bluetooth():
         if self.option == "on":
             result = self.__on()
             if not quiet:
-                if result == True:
+                if result:
                     print BLUETOOTH_ENABLED
                 else:
                     print BLUETOOTH_ENABLING_ERROR
         if self.option == "off":
             result = self.__off()
             if not quiet:
-                if result == True:
+                if result:
                     print BLUETOOTH_DISABLED
                 else:
                     print BLUETOOTH_DISABLING_ERROR
         if self.option == "toggle":
             result = self.__toggle()
             if not quiet:
-                if result == True:
+                if result:
                     # Temporary disable notifications
                     n = self.use_notify
                     self.use_notify = False
                     status = self.__status()
                     self.use_notify = n
                     # Notification re-enabled
-                    if status == True:
+                    if status:
                         print BLUETOOTH_ENABLED
                     else:
                         print BLUETOOTH_DISABLED
@@ -222,7 +224,7 @@ class Bluetooth():
                     toggle = False
             except:
                 pass
-            if toggle == True:
+            if toggle:
                 Bluetooth("toggle", self.use_notify).apply()
                 try:
                     file = open(tempfile, "w").close()  # create temp file
@@ -236,7 +238,7 @@ class Bluetooth():
         if self.option == "status":
             result = self.__status()
             if not quiet:
-                if result == True:
+                if result:
                     print BLUETOOTH_STATUS_ENABLED
                 else:
                     print BLUETOOTH_STATUS_DISABLED
@@ -249,7 +251,7 @@ class Cpu():
         self.use_notify = use_notify
         success = False
         retry = 3
-        while retry > 0 and success == False:
+        while retry > 0 and not success:
             try:
                 bus = dbus.SessionBus()
                 proxy = bus.get_object(
@@ -300,28 +302,28 @@ class Cpu():
         if self.option == "normal":
             result = self.__normal()
             if not quiet:
-                if result == True:
+                if result:
                     print FAN_STATUS_NORMAL
                 else:
                     print FAN_SWITCHING_ERROR
         if self.option == "silent":
             result = self.__silent()
             if not quiet:
-                if result == True:
+                if result:
                     print FAN_STATUS_SILENT
                 else:
                     print FAN_SWITCHING_ERROR
         if self.option == "overclock":
             result = self.__overclock()
             if not quiet:
-                if result == True:
+                if result:
                     print FAN_STATUS_OVERCLOCK
                 else:
                     print FAN_SWITCHING_ERROR
         if self.option == "cycle":
             result = self.__cycle()
             if not quiet:
-                if result == True:
+                if result:
                     # Temporary disable notifications
                     n = self.use_notify
                     self.use_notify = False
@@ -351,7 +353,7 @@ class Cpu():
                     hotkey = False
             except:
                 pass
-            if hotkey == True:
+            if hotkey:
                 Cpu("hotkey2", self.use_notify).apply()
                 try:
                     file = open(tempfile, "w").close()  # create temp file
@@ -405,7 +407,7 @@ class Webcam():
         self.use_notify = use_notify
         success = False
         retry = 3
-        while retry > 0 and success == False:
+        while retry > 0 and not success:
             try:
                 bus = dbus.SessionBus()
                 proxy = bus.get_object(
@@ -445,28 +447,28 @@ class Webcam():
         if self.option == "on":
             result = self.__on()
             if not quiet:
-                if result == True:
+                if result:
                     print WEBCAM_ENABLED
                 else:
                     print WEBCAM_ENABLING_ERROR
         if self.option == "off":
             result = self.__off()
             if not quiet:
-                if result == True:
+                if result:
                     print WEBCAM_DISABLED
                 else:
                     print WEBCAM_DISABLING_ERROR
         if self.option == "toggle":
             result = self.__toggle()
             if not quiet:
-                if result == True:
+                if result:
                     # Temporary disable notifications
                     n = self.use_notify
                     self.use_notify = False
                     status = self.__status()
                     self.use_notify = n
                     # Notification re-enabled
-                    if status == True:
+                    if status:
                         print WEBCAM_ENABLED
                     else:
                         print WEBCAM_DISABLED
@@ -485,7 +487,7 @@ class Webcam():
                     toggle = False
             except:
                 pass
-            if toggle == True:
+            if toggle:
                 Webcam("toggle", self.use_notify).apply()
                 try:
                     file = open(tempfile, "w").close()  # create temp file
@@ -499,7 +501,7 @@ class Webcam():
         if self.option == "status":
             result = self.__status()
             if not quiet:
-                if result == True:
+                if result:
                     print WEBCAM_STATUS_ENABLED
                 else:
                     print WEBCAM_STATUS_DISABLED
@@ -512,7 +514,7 @@ class Wireless():
         self.use_notify = use_notify
         success = False
         retry = 3
-        while retry > 0 and success == False:
+        while retry > 0 and not success:
             try:
                 bus = dbus.SessionBus()
                 proxy = bus.get_object(
@@ -552,28 +554,28 @@ class Wireless():
         if self.option == "on":
             result = self.__on()
             if not quiet:
-                if result == True:
+                if result:
                     print WIRELESS_ENABLED
                 else:
                     print WIRELESS_ENABLING_ERROR
         if self.option == "off":
             result = self.__off()
             if not quiet:
-                if result == True:
+                if result:
                     print WIRELESS_DISABLED
                 else:
                     print WIRELESS_DISABLING_ERROR
         if self.option == "toggle":
             result = self.__toggle()
             if not quiet:
-                if result == True:
+                if result:
                     # Temporary disable notifications
                     n = self.use_notify
                     self.use_notify = False
                     status = self.__status()
                     self.use_notify = n
                     # Notification re-enabled
-                    if status == True:
+                    if status:
                         print WIRELESS_ENABLED
                     else:
                         print WIRELESS_DISABLED
@@ -592,7 +594,7 @@ class Wireless():
                     toggle = False
             except:
                 pass
-            if toggle == True:
+            if toggle:
                 Wireless("toggle", self.use_notify).apply()
                 try:
                     file = open(tempfile, "w").close()  # create temp file
@@ -606,7 +608,7 @@ class Wireless():
         if self.option == "status":
             result = self.__status()
             if not quiet:
-                if result == True:
+                if result:
                     print WIRELESS_STATUS_ENABLED
                 else:
                     print WIRELESS_STATUS_DISABLED
@@ -723,7 +725,7 @@ def main():
     global quiet
     quiet = options.quiet
 
-    if options.status == True:
+    if options.status:
         options.backlight = "status"
         options.bluetooth = "status"
         options.cpu = "status"
@@ -761,7 +763,7 @@ def main():
     Webcam(options.webcam, options.show_notify).apply()
     Wireless(options.wireless, options.show_notify).apply()
 
-    if options.interface == True and not quiet:
+    if options.interface and not quiet:
         try:
             bus = dbus.SystemBus()
             proxy = bus.get_object(
@@ -780,7 +782,7 @@ def main():
             print unicode(_("Control interface: unable to connect to system service!"), "utf-8")
             pass
 
-    if options.stopsession == True:
+    if options.stopsession:
         try:
             bus = dbus.SessionBus()
             proxy = bus.get_object(
@@ -795,7 +797,7 @@ def main():
                 print unicode(_("Cannot stop session service"), "utf-8")
             pass
 
-    if options.stopsystem == True:
+    if options.stopsystem:
         try:
             bus = dbus.SystemBus()
             proxy = bus.get_object(

session-service.py
@@ -27,9 +27,6 @@ import dbus.service
 import dbus.mainloop.glib
 
 import gettext
-_ = gettext.gettext
-gettext.bindtextdomain("samsung-tools")
-gettext.textdomain("samsung-tools")
 
 from backends.globals import *
 from backends.session.options import Options
@@ -39,6 +36,10 @@ from backends.session.cpu import Cpu
 from backends.session.webcam import Webcam
 from backends.session.wireless import Wireless
 from backends.session.util.notifications import Notification
+
+_ = gettext.gettext
+gettext.bindtextdomain("samsung-tools")
+gettext.textdomain("samsung-tools")
 
 mainloop = None
 

backends/globals.py
@@ -20,6 +20,9 @@
 
 from backends.log import Log
 
+import sys
+import os.path
+
 APP_NAME = "Samsung Tools"
 APP_VERSION = "2.3.1"
 WORK_DIRECTORY = "/usr/share/samsung-tools"
@@ -129,8 +132,6 @@ PM_SCRIPTS = (PM_DEVICES_POWER_MANAGEMENT,
               PM_USB_AUTOSUSPEND,
               PM_VM_WRITEBACK_TIME)
 
-import sys
-import os.path
 ###
 # Stuff for session service only
 ###

backends/session/bluetooth.py
@@ -106,7 +106,7 @@ class Bluetooth(dbus.service.Object):
         if self.notify is not None and show_notify:
             self.notify.setTitle(BLUETOOTH_TITLE)
             self.notify.setUrgency("critical")
-            if result == True:
+            if result:
                 self.notify.setIcon(BLUETOOTH_ICON)
                 self.notify.setMessage(BLUETOOTH_ENABLED)
             else:
@@ -129,7 +129,7 @@ class Bluetooth(dbus.service.Object):
         if self.notify is not None and show_notify:
             self.notify.setTitle(BLUETOOTH_TITLE)
             self.notify.setUrgency("critical")
-            if result == True:
+            if result:
                 self.notify.setIcon(BLUETOOTH_ICON)
                 self.notify.setMessage(BLUETOOTH_DISABLED)
             else:

backends/session/cpu.py
@@ -151,7 +151,7 @@ class Cpu(dbus.service.Object):
             return False
         result = interface.SetNormal()
         if show_notify:
-            if result == True:
+            if result:
                 self.__show_notify(
                     CPU_TITLE, FAN_STATUS_NORMAL, FAN_NORMAL_ICON)
             else:
@@ -170,7 +170,7 @@ class Cpu(dbus.service.Object):
             return False
         result = interface.SetSilent()
         if show_notify:
-            if result == True:
+            if result:
                 self.__show_notify(
                     CPU_TITLE, FAN_STATUS_SILENT, FAN_SILENT_ICON)
             else:
@@ -189,7 +189,7 @@ class Cpu(dbus.service.Object):
             return False
         result = interface.SetOverclock()
         if show_notify:
-            if result == True:
+            if result:
                 self.__show_notify(
                     CPU_TITLE,
                     FAN_STATUS_OVERCLOCK,
@@ -211,7 +211,7 @@ class Cpu(dbus.service.Object):
         result = interface.Cycle()
         if show_notify:
             title = CPU_TITLE
-            if result == True:
+            if result:
                 status = interface.Status()
                 if status == 0:
                     message = FAN_STATUS_NORMAL

backends/session/options.py
@@ -72,7 +72,7 @@ class Options(dbus.service.Object):
     def SetUseHotkeys(self, value, sender=None, conn=None):
         """ Return 'True' on success, 'False' otherwise. """
         result = sessionconfig.setUseHotkeys(value)
-        if result == True:
+        if result:
             status = sessionconfig.getUseHotkeys()
             if status == "true":
                 self.hotkeys.setBacklightHotkey(
@@ -97,7 +97,7 @@ class Options(dbus.service.Object):
         """ Return 'True' on success, 'False' otherwise. """
         result = sessionconfig.setBacklightHotkey(hotkey)
         usehotkeys = sessionconfig.getUseHotkeys()
-        if result == True and usehotkeys == "true":
+        if result and usehotkeys == "true":
             self.hotkeys.setBacklightHotkey(hotkey)
         return result
 
@@ -107,7 +107,7 @@ class Options(dbus.service.Object):
         """ Return 'True' on success, 'False' otherwise. """
         result = sessionconfig.setBluetoothHotkey(hotkey)
         usehotkeys = sessionconfig.getUseHotkeys()
-        if result == True and usehotkeys == "true":
+        if result and usehotkeys == "true":
             self.hotkeys.setBluetoothHotkey(hotkey)
         return result
 
@@ -117,7 +117,7 @@ class Options(dbus.service.Object):
         """ Return 'True' on success, 'False' otherwise. """
         result = sessionconfig.setCpuHotkey(hotkey)
         usehotkeys = sessionconfig.getUseHotkeys()
-        if result == True and usehotkeys == "true":
+        if result and usehotkeys == "true":
             self.hotkeys.setCpuHotkey(hotkey)
         return result
 
@@ -127,7 +127,7 @@ class Options(dbus.service.Object):
         """ Return 'True' on success, 'False' otherwise. """
         result = sessionconfig.setWebcamHotkey(hotkey)
         usehotkeys = sessionconfig.getUseHotkeys()
-        if result == True and usehotkeys == "true":
+        if result and usehotkeys == "true":
             self.hotkeys.setWebcamHotkey(hotkey)
         return result
 
@@ -137,6 +137,6 @@ class Options(dbus.service.Object):
         """ Return 'True' on success, 'False' otherwise. """
         result = sessionconfig.setWirelessHotkey(hotkey)
         usehotkeys = sessionconfig.getUseHotkeys()
-        if result == True and usehotkeys == "true":
+        if result and usehotkeys == "true":
             self.hotkeys.setWirelessHotkey(hotkey)
         return result

backends/session/util/config.py
@@ -41,6 +41,7 @@ class SessionConfig():
         self.config = ConfigParser.SafeConfigParser()
         # Avoid to lowercase options names, this fixes Turkish locale
         self.config.optionxform = str
+        self.config.optionxform = str
         self.configfile = configfile
         try:
             self.config.readfp(open(configfile, "r"))
@@ -129,7 +130,8 @@ class SessionConfig():
                 shutil.copy(SESSION_CONFIG_FILE, self.configfile)
             except:
                 sessionlog.write(
-                    "ERROR: 'SessionConfig.__write()' - Cannot find the global user configuration file. Creating an empty one.")
+                    "ERROR: 'SessionConfig.__write()' - Cannot find the global user configuration file. " +
+                    "Creating an empty one.")
                 try:
                     oldfile = open(self.configfile, "w").close()
                 except:
@@ -173,9 +175,9 @@ class SessionConfig():
                         os.remove(self.configfile + ".new")
                         return False
         oldfile.close()
-        if sectionfound == False:  # probably an empty file, write section
+        if not sectionfound:  # probably an empty file, write section
             newfile.write("[Main]\n")
-        if optionfound == False:  # option not found in current config file, add it
+        if not optionfound:  # option not found in current config file, add it
             newfile.write(option + "=" + value + "\n")
         newfile.close()
         try:
@@ -219,7 +221,7 @@ class SessionConfig():
         """ Return 'True' on success, 'False' otherwise. """
         if value == "default":  # set default
             value = USE_HOTKEYS_DEFAULT
-        if not value in USE_HOTKEYS_ACCEPTED_VALUES:
+        if value not in USE_HOTKEYS_ACCEPTED_VALUES:
             return False
         self.config.set("Main", "USE_HOTKEYS", value)
         return self.__write("USE_HOTKEYS")

commit 9f59246ec2a8178fea73b9ba36ed1980d991808d

Fix localization issues caused by commit a0a7b32

samsung-tools-preferences.py
@@ -20,22 +20,22 @@
 # <http://www.gnu.org/licenses/gpl.txt>
 
 import os
+import sys
 import time
 import gtk
 import gobject
 import dbus
+
 import gettext
-import sys
+_ = gettext.gettext
+gettext.bindtextdomain("samsung-tools")
+gettext.textdomain("samsung-tools")
 
 WORK_DIRECTORY = "/usr/share/samsung-tools"
 sys.path.append(WORK_DIRECTORY)
 
 from backends.globals import *
 from backends.session.util.icons import *
-
-_ = gettext.gettext
-gettext.bindtextdomain("samsung-tools")
-gettext.textdomain("samsung-tools")
 
 
 class Popup (gtk.Window):

samsung-tools.py
@@ -21,12 +21,12 @@
 
 import os
 import sys
-
-from optparse import OptionParser
 import dbus
-import codecs
 
 import gettext
+_ = gettext.gettext
+gettext.bindtextdomain("samsung-tools")
+gettext.textdomain("samsung-tools")
 
 WORK_DIRECTORY = "/usr/share/samsung-tools"
 sys.path.append(WORK_DIRECTORY)
@@ -34,13 +34,10 @@ sys.path.append(WORK_DIRECTORY)
 from backends.globals import *
 from backends.session.util.locales import *
 
-_ = gettext.gettext
-gettext.bindtextdomain("samsung-tools")
-gettext.textdomain("samsung-tools")
-
 quiet = False
 
 # Fix encoding for piping
+import codecs
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 
@@ -669,6 +666,7 @@ def main():
         print unicode(_("Use --help for instructions."), "utf-8")
         sys.exit(1)
 
+    from optparse import OptionParser
     usage_string = unicode(_("Usage: %s <interface> <option> ...") %
                            os.path.basename(sys.argv[0]), "utf-8")
     parser = OptionParser(usage_string, add_help_option=False)

session-service.py
@@ -27,6 +27,9 @@ import dbus.service
 import dbus.mainloop.glib
 
 import gettext
+_ = gettext.gettext
+gettext.bindtextdomain("samsung-tools")
+gettext.textdomain("samsung-tools")
 
 from backends.globals import *
 from backends.session.options import Options
@@ -36,10 +39,6 @@ from backends.session.cpu import Cpu
 from backends.session.webcam import Webcam
 from backends.session.wireless import Wireless
 from backends.session.util.notifications import Notification
-
-_ = gettext.gettext
-gettext.bindtextdomain("samsung-tools")
-gettext.textdomain("samsung-tools")
 
 mainloop = None
 